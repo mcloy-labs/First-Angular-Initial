fc555e733543c67c0ad7b3dbeed8eb207897b8ec
mcloy-labs <kharystephenson912@gmail.com>
2024-10-17 20:30:58 -0300

    chore: rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-aside_wireframe' title`, () => {
+  it(`should have the 'angular-aside_starter' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-aside_wireframe');
+    expect(app.title).toEqual('angular-aside_starter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-aside_wireframe');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-aside_starter');
   });
 });
