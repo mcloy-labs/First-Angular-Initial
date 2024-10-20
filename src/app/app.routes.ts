682b4dd18c0350ef5c6f575ba778faa3145a414a
mcloy-labs <kharystephenson912@gmail.com>
2024-10-20 13:05:47 -0300

    chore: fix page name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-	{ path: 'dashboard', loadComponent: () => import('./pages/wellcome/wellcome.component').then(m => m.WellcomeComponent) },
+	{ path: 'dashboard', loadComponent: () => import('./pages/welcome/welcome.component').then(m => m.WelcomeComponent) },
 	{ path: 'settings', loadComponent: () => import('./pages/settings/settings.component').then(m => m.SettingsComponent) },
 
 	{ path: '404', loadComponent: () => import('./pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) },
